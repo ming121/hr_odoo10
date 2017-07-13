1a3dc2721b40a26bb5d03e5709c8d7ed58484d37
ming <ming_dekoomyim@hotmail.com>
2017-07-13 22:44:39 +0700

    บวก

--- hr_payeoll_thai/models/hr_payroll_th.py
+++ hr_payeoll_thai/models/hr_payroll_th.py
@@ -109,87 +109,57 @@ class hr_payroll_th_line(models.Model):
     tax = fields.Float(string="ภาษ๊หัก ณ ที่จ่าย", digits=dp.get_precision('Payroll'), store=True)
     other_broken = fields.Float(string="หัก อื่นๆ", digits=dp.get_precision('Payroll'), store=True)
     amount_sso = fields.Float(
-        # compute='_compute_sso',
+        compute='_compute_sso',
         string='เงินสมบท', digits=dp.get_precision('Payroll'), store=True)
     rate = fields.Float(string='Rate (%)', digits=dp.get_precision('Payroll Rate'), related='sso_id.contribution_rate',
                         store=True)
     total_deduction = fields.Float(
-        # compute='_compute_deduction',
+        compute='_compute_deduction',
         string="รายการหักรวม", digits=dp.get_precision('Payroll'), store=True)
 
     pay_total = fields.Float(
-        # compute='_compute_pay_total',
+        compute='_compute_pay_total',
         string="สรุปรายได้", digits=dp.get_precision('Payroll'), store=True)
 
     sso_id = fields.Many2one('hr.sso.config', string='SSO', index=True, copy=False, )
 
 
 
-#
-# class hr_payroll_th_line_deduction(models.Model):
-#     _name = "hr.payroll.th.line.deduction"
-#
-#
-#
-#
-#
-#     payroll_deduction_id = fields.Many2one('hr.payroll.th', string='Invoice Reference',
-#                                       ondelete='cascade', index=True)
-#
-#     employee_ids = fields.Many2one('hr.employee', ondelete='restrict',
-#                                    string="Employees", required=True)
-#
-#     tax = fields.Float(string="ภาษ๊หัก ณ ที่จ่าย", digits=dp.get_precision('Payroll'), store=True)
-#     other_broken = fields.Float(string="หัก อื่นๆ", digits=dp.get_precision('Payroll'), store=True)
-#     amount_sso = fields.Float(
-#                                 # compute='_compute_sso',
-#                                 string='เงินสมบท',digits=dp.get_precision('Payroll'),store=True)
-#     rate = fields.Float(string='Rate (%)', digits=dp.get_precision('Payroll Rate') ,related='sso_id.contribution_rate',store=True)
-#     total_deduction =fields.Float(
-#                                     # compute='_compute_deduction',
-#                                     string="รายการหักรวม",digits=dp.get_precision('Payroll'),store=True)
-#
-#     pay_total =fields.Float(
-#                                 # compute='_compute_pay_total',
-#                                 string="สรุปรายได้",digits=dp.get_precision('Payroll'),store=True)
-#
-#     sso_id = fields.Many2one('hr.sso.config', string='SSO', index=True, copy=False, )
-
-    # rate_maximun_wage = fields.Float(related='sso_id.maximun_wage', store=True)
-    # rate_minimun_wage = fields.Float(related='sso_id.minimun_wage', store=True)
-    # rate_sso = fields.Float(string='อัตราเงินสมทบ', related='sso_id.contribution_rate', store=True, copy=False)
+    rate_maximun_wage = fields.Float(related='sso_id.maximun_wage', store=True)
+    rate_minimun_wage = fields.Float(related='sso_id.minimun_wage', store=True)
+    rate_sso = fields.Float(string='อัตราเงินสมทบ', related='sso_id.contribution_rate', store=True, copy=False)
     #
 
-    #
-    # @api.multi
-    # @api.depends('salary_base', 'quantity_day', 'rate','rate_maximun_wage','rate_minimun_wage')
-    # def _compute_sso(self):
-    #     for line in self:
-    #         sum_pay = float((line.quantity_day * line.salary_base))
-    #         if  sum_pay <= line.rate_minimun_wage :
-    #             self.amount_sso = round(( line.rate_minimun_wage * line.rate) / 100)
-    #             return
-    #
-    #         elif sum_pay  >= line.rate_maximun_wage :
-    #             self.amount_sso = round((line.rate_maximun_wage * line.rate) / 100)
-    #             return
-    #         else  :
-    #             self.amount_sso = round( (sum_pay * line.rate )/ 100)
-    #             return
-    #
 
-    #
-    # @api.multi
-    # @api.depends('tax', 'other_broken', 'amount_sso')
-    # def _compute_deduction(self):
-    #     for line in self:
-    #         line.total_deduction = float(line.tax + line.other_broken + line.amount_sso )
-    #
-    # @api.multi
-    # @api.depends('total_receipts', 'total_deduction')
-    # def _compute_pay_total(self):
-    #     for line in self:
-    #         line.pay_total = float(line.total_receipts -  line.total_deduction)
+    @api.multi
+    @api.depends('salary_base', 'quantity_day', 'rate','rate_maximun_wage','rate_minimun_wage')
+    def _compute_sso(self):
+        for line in self:
+            sum_pay = float((line.quantity_day * line.salary_base))
+            if  sum_pay <= line.rate_minimun_wage :
+                self.amount_sso = round(( line.rate_minimun_wage * line.rate) / 100)
+                return
+
+            elif sum_pay  >= line.rate_maximun_wage :
+                self.amount_sso = round((line.rate_maximun_wage * line.rate) / 100)
+                return
+            else  :
+                self.amount_sso = round( (sum_pay * line.rate )/ 100)
+                return
+
+
+
+    @api.multi
+    @api.depends('tax', 'other_broken', 'amount_sso')
+    def _compute_deduction(self):
+        for line in self:
+            line.total_deduction = float(line.tax + line.other_broken + line.amount_sso )
+
+    @api.multi
+    @api.depends('total_receipts', 'total_deduction')
+    def _compute_pay_total(self):
+        for line in self:
+            line.pay_total = float(line.total_receipts -  line.total_deduction)
 
 
 
